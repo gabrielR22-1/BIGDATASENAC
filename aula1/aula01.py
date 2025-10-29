273260943f9d10c979402fe85e994e65f21ec50e
GABRIEL RAMIRES FERREIRA <gabriel.ramires@aluno.senac.rio>
2025-10-29 19:10:33 -0300

    adição conteudo aula04

---
+++ aula1/aula01.py
@@ -0,0 +1,25 @@
+'''AULA 01 - VARIÁVEIS - 22/10/2025'''
+
+## Categorias
+idade=30 #int
+nome='maria' #string
+preco=19.99 #float
+estoque=False #boolean
+conjunto=None #nulo
+print(idade)
+print(type(idade))
+print(nome)
+print(type(nome))
+print(preco)
+print(type(19.99))
+print(estoque)
+print(type(estoque))
+print(conjunto)
+print(type(conjunto))
+
+cadastro=float(input('Digite seu numero:  '))
+print(cadastro)
+print(type(cadastro))
+
+#Input
+#OutPut
